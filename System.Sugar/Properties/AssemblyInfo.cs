fea6495cd414ac6c0754d6ca77205e8a02c2a2b9
Groma István <groma.istvan@sdadms.hu>
2016-02-19 13:56:39 +0100

    Useful .NET extensions.

---
+++ System.Sugar/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct( "System.Sugar" )]
+[assembly: AssemblyTitle( "System.Sugar" )]
+[assembly: AssemblyDescription( "Useful .NET extensions." )]
+[assembly: AssemblyCompany( "MGR-Infó Bt." )]
+[assembly: AssemblyCopyright( "Copyright © MGR-Infó Bt. 2016" )]
+
+[assembly: ComVisible( false ), Guid( "a58f56be-44fb-4cce-b44b-560bbdf73a87" )]
+[assembly: CLSCompliant( true )]
+
+[assembly: AssemblyVersion( "1.0.*" )]
+#if DEBUG
+[assembly: AssemblyInformationalVersion( "1.0.0-alpha" )]
+#else
+[assembly: AssemblyInformationalVersion( "1.0.0" )]
+#endif
---
+++ System.Sugar/SuperString.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Sugar
+{
+    using Text;
+
+    /// <summary>
+    /// Fancy class for using smart string manipulation.
+    /// </summary>
+    public class SuperString
+    {
+        /// <summary>
+        /// Used for implementation logic.
+        /// </summary>
+        protected StringBuilder Internal { get; } = new StringBuilder();
+    }
+}
